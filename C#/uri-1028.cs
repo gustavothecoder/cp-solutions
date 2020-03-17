4ddcdb0be6684ce6caa1effe16db47a00f34d009
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-03-17 20:57:56 -0300

    solved

---
+++ C#/uri-1028.cs
@@ -0,0 +1,40 @@
+﻿// URI - 1027: https://www.urionlinejudge.com.br/judge/pt/problems/view/1027
+
+using System;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfExchanges = int.Parse(Console.ReadLine());
+            string[] numberOfStickers = new string[2];
+            int ricardoStickers, vicenteStickers;
+
+            for (int i = 0; i < numberOfExchanges; i++)
+            {
+                numberOfStickers = Console.ReadLine().Split(' ');
+
+                ricardoStickers = int.Parse(numberOfStickers[0]);
+                vicenteStickers = int.Parse(numberOfStickers[1]);
+
+                Console.WriteLine(GreatestCommonDivisor(ricardoStickers, vicenteStickers));
+            }
+        }
+
+        private static int GreatestCommonDivisor(int ricardoStickers, int vicenteStickers)
+        {
+            int remainder;
+
+            while (vicenteStickers != 0)
+            {
+                remainder = ricardoStickers % vicenteStickers;
+                ricardoStickers = vicenteStickers;
+                vicenteStickers = remainder;
+            }
+
+            return ricardoStickers;
+        }
+    }
+}
