0ed513a3199390cb3f92985aa89cd2de9eb46157
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-03-16 21:05:34 -0300

    solved

---
+++ C#/uri-1026.cs
@@ -0,0 +1,28 @@
+﻿// URI - 1026: https://www.urionlinejudge.com.br/judge/pt/problems/view/1026
+
+using System;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = new string[2];
+
+            while (true)
+            {
+                try
+                {
+                    numbers = Console.ReadLine().Split(' ');
+
+                    Console.WriteLine(uint.Parse(numbers[0]) ^ uint.Parse(numbers[1]));
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
