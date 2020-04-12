c826e245b4ebe3f19b18fd498da82a7363f220c6
Gustavo Ribeiro <57065994+Dev-GustavoRibeiro@users.noreply.github.com>
2020-04-12 09:33:21 -0300

    Update uri-2602.sql

--- SQL/uri-2602.sql
+++ SQL/uri-2602.sql
@@ -1,7 +1,7 @@
 /* URI - 2602: https://www.urionlinejudge.com.br/judge/pt/problems/view/2602 */
 
 CREATE TABLE customers(
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
     street VARCHAR(50) NOT NULL,
     city VARCHAR(45) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE customers(
 );
 
 INSERT INTO customers(name, street, city, state, credit_limit) VALUES('Pedro Augusto da Rocha', 'Rua Pedro Carlos Hoffman', 'Porto Alegre', 'RS', 700.00),
-																	 ('Antonio Carlos Mamel', 'Av. Pinheiros', 'Belo Horizonte', 'MG', 3500.50),
+								     ('Antonio Carlos Mamel', 'Av. Pinheiros', 'Belo Horizonte', 'MG', 3500.50),
                                                                      ('Luiza Augusta Mhor', 'Rua Salto Grande', 'Niteroi', 'RJ', 4000.00),
                                                                      ('Jane Ester', 'Av. 7 de setembro', 'Erechim', 'RS', 800.00),
                                                                      ('Marcos Antônio dos Santos', 'Av. Farrapos', 'Porto Alegre', 'RS', 4250.25);
