0fa47462a03c41e6a0e98cda0864c9eba762adc6
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-07-06 23:38:10 -0300

    Solved

---
+++ Ruby/uri-1005.rb
@@ -0,0 +1,6 @@
+#URI - 1005: https://www.urionlinejudge.com.br/judge/pt/problems/view/1005
+
+A = gets.to_f
+B = gets.to_f
+Average = ((A * 3.5) + (B * 7.5)) / 11
+puts "MEDIA = #{'%.5f' % Average}"
