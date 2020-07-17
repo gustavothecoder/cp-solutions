8ed3c30f8e8a0bb664dd1d2e0c27d25d867a2e42
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-07-16 23:37:13 -0300

    Solved

---
+++ Ruby/uri-1006.rb
@@ -0,0 +1,7 @@
+#URI - 1006: https://www.urionlinejudge.com.br/judge/pt/problems/view/1006
+
+A = gets.to_f
+B = gets.to_f
+C = gets.to_f
+Average = ((A * 2) + (B * 3) + (C * 5)) / 10
+puts "MEDIA = #{Average.round(1)}"
