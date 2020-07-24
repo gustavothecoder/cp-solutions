9c05ac4c2e44d62deee2a0f83bf00cc38cdf8707
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-07-23 23:31:24 -0300

    Solved

---
+++ Ruby/uri-1007.rb
@@ -0,0 +1,8 @@
+#URI - 1007: https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
+
+A = gets.to_i
+B = gets.to_i
+C = gets.to_i
+D = gets.to_i
+Result = (A * B - C * D)
+puts "DIFERENCA = #{Result}"
