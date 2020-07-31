64370ebdd9f1a50150d6d95dc050caa4d3677668
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-07-30 23:39:55 -0300

    Solved

---
+++ C#/uri-1035.cs
@@ -0,0 +1,27 @@
+﻿// URI - 1035: https://www.urionlinejudge.com.br/judge/pt/problems/view/1035
+
+using System;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int valueA = int.Parse(values[0]);
+            int valueB = int.Parse(values[1]);
+            int valueC = int.Parse(values[2]);
+            int valueD = int.Parse(values[3]);
+            if (valueB > valueC
+                && valueD > valueA
+                && valueC + valueD > valueA + valueB
+                && valueC >= 0
+                && valueD >= 0
+                && valueA % 2 == 0)
+                Console.WriteLine("Valores aceitos");
+            else
+                Console.WriteLine("Valores nao aceitos");
+        }
+    }
+}
