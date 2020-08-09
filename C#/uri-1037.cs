954b9066d47fd0d752d2f0efa277a40375f1635a
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-08-09 16:18:52 -0300

    Solved

---
+++ C#/uri-1037.cs
@@ -0,0 +1,24 @@
+﻿// URI - 1037: https://www.urionlinejudge.com.br/judge/pt/problems/view/1037
+
+using System;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            if (input < 0 || input > 100)
+                Console.WriteLine("Fora de intervalo"); 
+            else if (input <= 25)
+                Console.WriteLine("Intervalo [0,25]");
+            else if (input <= 50)
+                Console.WriteLine("Intervalo (25,50]");
+            else if (input <= 75)
+                Console.WriteLine("Intervalo (50,75]");
+            else
+                Console.WriteLine("Intervalo (75,100]");
+        }
+    }
+}
