554dcc7b1e83bf5f6c67050c83d63ae17ad5e880
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-09 19:15:41 -0300

    Resolve the uri-1042 with Ruby

---
+++ Ruby/uri-1042.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# URI - 1042: https://www.urionlinejudge.com.br/judge/pt/problems/view/1042
+
+values = gets.split(' ').map(&:to_i)
+puts values.sort
+puts ''
+puts values
