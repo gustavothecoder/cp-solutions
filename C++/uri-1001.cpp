b9fe8995cf8550c24e56c736b1dc2d7f68b4153c
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-04-12 21:48:23 -0300

    Resolve the URI 1001 with C++

---
+++ C++/uri-1001.cpp
@@ -0,0 +1,14 @@
+// URI-1001: https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, x;
+  cin >> a;
+  cin >> b;
+  cout << "X = " << a + b << endl;
+  
+  return 0;
+}
