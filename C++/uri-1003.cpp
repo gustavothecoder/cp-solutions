7ce4fa3e20fabe837cead786d44cc57e91c1730d
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-04-18 20:41:44 -0300

    Resolve the URI 1003 with C++

---
+++ C++/uri-1003.cpp
@@ -0,0 +1,13 @@
+// URI-1003: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a;
+  cin >> b;
+  cout << "SOMA = " << a + b << endl;
+  return 0;
+}
