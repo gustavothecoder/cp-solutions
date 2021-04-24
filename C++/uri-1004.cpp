451fa4899592acb2614907bb6bf2a2d6828eff66
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-04-24 20:14:53 -0300

    Resolve the URI 1004 with C++

---
+++ C++/uri-1004.cpp
@@ -0,0 +1,13 @@
+// URI 1004: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a;
+  cin >> b;
+  cout << "PROD = " << a * b << endl;
+  return 0;
+}
