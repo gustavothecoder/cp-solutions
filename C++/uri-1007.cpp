0fca8c60ba9ba474803aa5c158680b1e3c2d4b7f
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-05-09 20:08:05 -0300

    Resolve the URI 1007 with C++

---
+++ C++/uri-1007.cpp
@@ -0,0 +1,15 @@
+// URI 1007: https://www.urionlinejudge.com.br/judge/en/problems/view/1007
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  cin >> d;
+  cout << "DIFERENCA = " << (a*b) - (c*d) << endl;
+  return 0; 
+}
