f77b07b78e8d20752e0ac3bfa0f55c612538fe10
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-05-15 20:26:40 -0300

    Resolve the URI 1035 with C++

---
+++ C++/uri-1035.cpp
@@ -0,0 +1,16 @@
+// URI 1035: https://www.urionlinejudge.com.br/judge/en/problems/view/1035
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (b > c && d > a && (c + d) > (a + b) && (c >=0 && d >= 0) && a % 2 == 0)
+    cout << "Valores aceitos" << endl;
+  else
+    cout << "Valores nao aceitos" << endl;
+
+  return 0;
+}
